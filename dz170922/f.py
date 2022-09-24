b0c54125af870da799abfe6da4465cec13cd5ba4
ladikmew <113600732+ladikmew@users.noreply.github.com>
2022-09-24 11:43:07 +0500

    Update f.py

--- dz170922/f.py
+++ dz170922/f.py
@@ -1,15 +1,23 @@
-a = input().split()
-n = int(a[0])
-k = int(a[1])
-s = input().split()
-f = 0
-for i in range(0,n):
-    if int(a[i])>k:
-        f+=1
-        break
-for j in range(n-1, i+1, -1):
-    if int(a[i]) > k:
-        f += 1
-        break
-print(f)
 
+
+def f1(a: list, k: int) -> int:
+    f = 0
+    n = len(a)
+    for i in range(0, n):
+        if a[i] > k:
+            break
+        else:
+            f += 1
+    for j in range(n-1, i, -1):
+        if a[i] > k:
+            break
+        else:
+            f += 1
+    return f
+
+
+if __name__ == "__main__":
+    n, k = map(int, input().split())
+    s = list(map(int, input().split()))
+    r = f1(s, k)
+    print(r)

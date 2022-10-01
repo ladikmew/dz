246de27cae361ea34ef0d9b32e353c69539da76a
ladikmew <113600732+ladikmew@users.noreply.github.com>
2022-10-01 11:14:01 +0500

    Update 10.09.22.py

--- 10.09.22.py
+++ 10.09.22.py
@@ -1,10 +1,15 @@
-f1 = 1
-f2 = 1
-a = int(input())
-k = 0
-while k < (a-2):
-    summ = f1+f2
-    f1 = f2
-    f2 = summ
-    k+=1
-print(f2)
+def fib(f1, f2, a, k):
+    while k < (a - 2):
+        s = f1 + f2
+        f1 = f2
+        f2 = s
+        k += 1
+    print(f2)
+
+
+if __name__ == "__main__":
+    f1 = 1
+    f2 = 1
+    a = int(input())
+    k = 0
+    fib(f1, f2, a, k)

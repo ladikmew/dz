5115267b1b83d08d29ee1506502efc1dc84c0db9
ladikmew <113600732+ladikmew@users.noreply.github.com>
2022-12-19 16:17:15 +0500

    Update marks.py

--- dz220909/marks.py
+++ dz220909/marks.py
@@ -9,7 +9,7 @@ d = [
     },
 
     {
-        "name": "Dima",
+        "name": "Stas",
         "value": 3,
         "klass": "10N",
     },
@@ -25,10 +25,15 @@ data = pd.DataFrame(d)
 
 data.to_csv("klass.csv")
 
+def get_name(klass, value):
+    if "10N" in d and c == "5":
+        s.append(b)
+
 
 with (open("klass.csv", "r")) as f:
     for line in f:
         a, b, c, d = map(str, line.split(','))
-        if "10N" in d and c == "5":
-            print(b)
+        s = input().split()
+        get_name(c,a)
+print(*s)
 

32224db6e9dfd618a7cb9990c6f597c4bf63f4f9
ladikmew <113600732+ladikmew@users.noreply.github.com>
2023-02-04 08:34:46 +0500

    Update dz.py

--- dz230127/dz.py
+++ dz230127/dz.py
@@ -1,31 +1,56 @@
 from typing import Dict, List
-
-
+from collections import deque
 def dfs(al: Dict[str, List[str]], start: str, finish: str,visited: List[str]) -> List[str]:
-    if al[start]==finish:
+        if start == finish:
         return True
-    visited.append(al[start])
+    if start in vizited:
+        return False
+    vizited += start
+    for n in al[start]:
+        if not n in vizited:
+            if dfs(n, finish,al,vizited):
+                return True
     return False
-    for i in al[start] - visited:
-        dfs(al: Dict[str, List[str]], i: str, finish: str,visited: List[str])
-    return True
+    
+    
+    
+#     if al[start]==finish:
+#         return True
+#     visited.append(al[start])
+#     return False
+#     for i in al[start] - visited:
+#         dfs(al: Dict[str, List[str]], i: str, finish: str,visited: List[str])
+#     return True
 
-    # реализация через рекурсию или стек
+#     реализация через рекурсию или стек
 #     pass
-#
-#
-# def bfs(al: Dict[str, List[str]], start: str, finish: str) -> List[str]:
-#     # реализация через очередь
-#     pass
-#
-#     # dz_test.py
-#     # main.py
-#
-#
-# def main(filename: str, from_city: str, to_city: str):
-#
-#
-# # напечатать путь из города from_city до города to_city либо что его нет.
+
+
+def bfs(al: Dict[str, List[str]], start: str, finish: str) -> List[str]:
+    squeue = deque()
+    squeue+=al[start]
+    vizited = []
+    while squeue:
+        node = squeue.popleft()
+        if not node in vizited:
+            if node == search:
+                return True
+            else:
+                squeue+=graf[node]
+                vizited+=[node]
+    return False
+    
+    # реализация через очередь
+    pass
+
+    # dz_test.py
+    # main.py
+
+
+def main(filename: str, from_city: str, to_city: str):
+
+
+# напечатать путь из города from_city до города to_city либо что его нет.
 
 if __name__ == "__main__":
     # python main.py adjacency_list.txt москва екатеринбург

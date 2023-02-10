411ffd9340831ed597cbda51fc5a6a2732f7845e
ladikmew <113600732+ladikmew@users.noreply.github.com>
2023-02-10 13:39:18 +0500

    Update struct_data_test.py

--- dz230114/struct_data_test.py
+++ dz230114/struct_data_test.py
@@ -3,41 +3,63 @@ from struct_data import DoubleLinkedList
 from struct_data import ArrayList
 from struct_data import Queue
 from struct_data import Stack
+import unittest
 
 
 class TestDoubleLinkedList(TestCase):
 
     def test_append(self):
         self.a = DoubleLinkedList()
-        l = self.a.len()
+        l = self.a.len
         self.a.append(5)
-        self.assertEqual(self.a.len(), l + 1)
+        self.assertEqual(self.a.len, l + 1)
+        self.assertEqual(self.a.geti(self.a.finish), 5)
 
     def test_len(self):
         self.a = DoubleLinkedList()
-        r = len(self.a)
-        self.assertEqual(self.a.len(), r)
+        for i in range(10):
+            self.a.append(i)
+        self.assertEqual(self.a.len, 10)
+
+    def test_len2(self):
+        self.a = DoubleLinkedList()
+        for i in range(10):
+            self.a.append(i)
+        for j in range(5):
+            self.a.remove(self.a.finish)
+        self.assertEqual(self.a.len, 5)
 
     def test_insert(self):
-        self.a = [1, 2, 3, 4]
-        # self.a.append(1)
-        self.a.insert(0, 5)
-        l = [5, 1, 2, 3, 4]
-        self.assertEqual(self.a, l)
+        self.a = DoubleLinkedList()
+        for el in [1, 2, 3, 4]:
+            self.a.append(el)
+        ind = self.a.search(0)
+        self.a.insert(ind, 5)
+        self.assertEqual(self.a.geti(self.a.start), 5)
 
     def test_search(self):
         self.a = DoubleLinkedList()
-        self.a.append(1)
-        self.a.search(0)
-        l = self.a[0]
-        self.assertEqual(self.a, l)
+        for el in [1, 2, 3, 4]:
+            self.a.append(el)
+        k = self.a.search(0)
+        self.assertEqual(self.a.geti(k), 1)
+
+    def test_search2(self):
+        self.a = DoubleLinkedList()
+        for el in [1, 2, 3, 4]:
+            self.a.append(el)
+        for el in range(2):
+            self.a.remove(self.a.finish)
+        for el in [5, 6, 7, 8]:
+            self.a.append(el)
+        k = self.a.search(3)
+        self.assertEqual(self.a.geti(k), 6)
 
     def test_geti(self):
         self.a = DoubleLinkedList()
         self.a.append(1)
-        self.a.geti(0)
-        l = self.a[0]
-        self.assertEqual(self.a, l)
+        h = self.a.geti(self.a.start)
+        self.assertEqual(h, 1)
 
 
 class TestArrayList(TestCase):
@@ -115,3 +137,7 @@ class TestStack(TestCase):
         r = self.a[len(self.a) - 1]
         self.a.top()
         self.assertEqual(self.a, r)
+
+
+if __name__ == "__main__":
+    unittest.main()

5623c199f1af166dc4f39021ad0d8d2095b0437a
ladikmew <113600732+ladikmew@users.noreply.github.com>
2023-05-19 10:53:51 +0500

    Update dz.py

--- dz230127/dz.py
+++ dz230127/dz.py
@@ -1,53 +1,53 @@
 from typing import Dict, List
 from collections import deque
-def dfs(al: Dict[str, List[str]], start: str, finish: str,visited: List[str]) -> List[str]:
-        if start == finish:
-        return True
-    if start in vizited:
-        return False
-    vizited += start
-    for n in al[start]:
-        if not n in vizited:
-            if dfs(n, finish,al,vizited):
-                return True
-    return False
-    
-    
-    
-#     if al[start]==finish:
+# def dfs(al: Dict[str, List[str]], start: str, finish: str,visited: List[str]) -> List[str]:
+#         if start == finish:
 #         return True
-#     visited.append(al[start])
+#     if start in vizited:
+#         return False
+#     vizited += start
+#     for n in al[start]:
+#         if not n in vizited:
+#             if dfs(n, finish,al,vizited):
+#                 return True
 #     return False
-#     for i in al[start] - visited:
-#         dfs(al: Dict[str, List[str]], i: str, finish: str,visited: List[str])
-#     return True
 
-#     реализация через рекурсию или стек
-#     pass
-
-
-def bfs(al: Dict[str, List[str]], start: str, finish: str) -> List[str]:
-    squeue = deque()
-    squeue+=al[start]
-    vizited = []
-    while squeue:
-        node = squeue.popleft()
-        if not node in vizited:
-            if node == search:
-                return True
-            else:
-                squeue+=graf[node]
-                vizited+=[node]
-    return False
     
-    # реализация через очередь
-    pass
+    
+    
+def bfs(start, finish, graph):
+    queue = deque([start])
+    visited = {start}
 
-    # dz_test.py
-    # main.py
+    while queue:
+        current = queue.popleft()
+        if current == finish:
+            break
+
+        nnodes = graph[current]
+        for nnode in nnodes:
+            if nnode not in visited:
+                queue.append(nnode)
+                visited[nnode] = current
+    return visited
+
+# def bfs(al: Dict[str, List[str]], start: str, finish: str) -> List[str]:
+#     squeue = deque()
+#     squeue+=al[start]
+#     vizited = []
+#     while squeue:
+#         node = squeue.popleft()
+#         if not node in vizited:
+#             if node == search:
+#                 return True
+#             else:
+#                 squeue+=graf[node]
+#                 vizited+=[node]
+#     return False
 
 
 def main(filename: str, from_city: str, to_city: str):
+        pass
 
 
 # напечатать путь из города from_city до города to_city либо что его нет.

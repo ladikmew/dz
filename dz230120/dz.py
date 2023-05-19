4b931f8358bd93bb11ca0ea48717b5ad5b31cb39
ladikmew <113600732+ladikmew@users.noreply.github.com>
2023-05-19 10:48:02 +0500

    Update dz.py

--- dz230120/dz.py
+++ dz230120/dz.py
@@ -1,35 +1,63 @@
 from collections import deque
+
+
 def unique_numbers(l: list) -> list:
-    minel = 0
-    maxel = 0
-    m = []
-    t = 10000000000000000
-    t1 = -10000000000000000
-    for i in range(len(l)): # находим минимальный и максимальный элементы массива
-        if l[i] < t:
-            t = l[i]
-            minel = l[i]
-        if l[i] > t1:
-            t1 = l[i]
-            maxel = l[i]
-    for i in range((maxel - minel)+1):# заполнили новый массив нулями
-        m.append(0)
-    for i in range(len(l)):# каждому элементу исходного массива находим место в новом, по формуле и прибавляем еденичку
-        f = int(l[i])-minel
-        m[f] +=1
+    """
+    Возвращение списка  всех неповторяющихся элемнтов входного списка
+    """
+    if not l:
+        return []
+    minel = min(l)
+    maxel = max(l)
+    m = [0] * ((maxel - minel) + 1)  # заполнили новый массив нулями
+    # каждому элементу исходного массива находим место в новом, по формуле и прибавляем еденичку
+    for el in l:
+        m[el - minel] += 1
     rez = []
-    for i in range(len(l)):# идём по исходному массиву и если в новом массиве на его формульном месте число равно еденичке то этот элемент встречается в исходном один раз
-        if m[int(l[i])-minel]==1:
-            rez.append(int(l[i]))
+    # идём по исходному массиву и
+    # если в новом массиве на его формульном месте число равно еденичке
+    # то этот элемент встречается в исходном один раз
+    for el in l:
+        if m[el - minel] == 1:
+            rez.append(el)
 
     return rez
 
+
 def sort_stack(s: deque) -> None:
+    """
+    Сртировка стека с помощью дополнительных стеков
+    """
+    dop1 = deque()  # левая
+    dop2 = deque()  # средняя
+    dop3 = deque()  # правая
+
+    while len(s)>0:
+        l1 = s.pop()
+        l2 = None
+        while len(dop3) > 0:
+            l2 = dop3.pop()
+            if l1 <= l2:
+                break
+            else:
+                dop1.append(l2)
+                l2 = None
+        if l2 is not None:
+            dop3.append(l2)
+        dop3.append(l1)
+        while len(dop1) > 0:
+            dop3.append(dop1.pop())
+
+    while len(dop3)>0:
+        dop2.append(dop3.pop())
+    while len(dop2)>0:
+        s.append(dop2.pop())
+    
 
 
-
-
-if __name__=="__main__":
-    l = list(map(int,input().split()))
-    print(unique_numbers(l))
-    s = deque()
+if __name__ == "__main__":
+    # l = list(map(int, input().split()))
+    # print(unique_numbers(l))
+    s = deque([5, 4, 6, 1])
+    sort_stack(s)
+    print(s)

5b2b4cba3e789060be4f85711fd4ab34092de184
ladikmew <113600732+ladikmew@users.noreply.github.com>
2023-05-30 00:45:19 +0500

    Update main_112614_N.py

--- dz230408/main_112614_N.py
+++ dz230408/main_112614_N.py
@@ -1,12 +1,10 @@
-#обратный порядок
+#прямой порядок
 n, m = list(map(int, input().split()))
-a = [[0] * m for i in range(n)]
+w = max(n,m)
+a = [[0] * (w+1) for i in range(w+1)]
 a[0][0] = 1
-for i in range(1, n):
-    for j in range(1, m):
-        k = a[i - 1][j - 2]
-        l = a[i - 2][j - 1]
-        a[i][j] += k
-        a[i][j] += l
-        
+for i in range(0, n - 1):
+    for j in range(0, m - 1):
+        a[i + 1][j + 2] += a[i][j]
+        a[i + 2][j + 1] += a[i][j]
 print(a[n - 1][m - 1])

e1701dddb4befbd1075a9cd13d947f7d5fc52549
ladikmew <113600732+ladikmew@users.noreply.github.com>
2023-05-30 10:10:39 +0500

    Update 112601_2.py

--- dz230407/112601_2.py
+++ dz230407/112601_2.py
@@ -1,20 +1,18 @@
 def fibonachi(n):
-    if n>6:
-        fib = [0]*(n)
+
+    if n == 1:
+        return 2
     else:
-        fib = [0]*(n+2)
-    fib[0] = 1
-    i = 0
-    r = 0
-    while r < n:
-        fib[i + 1] += fib[i]
-        fib[i + 2] += fib[i]
-        r = fib[i+1]
-        i += 1
-    if r==n:
-        return i+1
-    else:
-        return -1
+        fib = [0]*100
+        fib[1] = 1
+        for i in range(1, 100):
+            fib[i + 1] += fib[i]
+            fib[i + 2] += fib[i]
+
+            if fib[i] == n:
+                return i
+            elif fib[i]>n:
+                return -1
 
 if __name__ == "__main__":
     n = int(input())

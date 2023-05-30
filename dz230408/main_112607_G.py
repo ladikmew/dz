2525cd1962131494ecb6881129414eae2984e7dd
ladikmew <113600732+ladikmew@users.noreply.github.com>
2023-05-30 07:53:22 +0500

    Update main_112607_G.py

--- dz230408/main_112607_G.py
+++ dz230408/main_112607_G.py
@@ -1,11 +1,12 @@
-n = int(input())
-k = n-1
-r = 0
-jump = [0]*(n+k)
-jump[0] = 1
-for i in range(0,n):
-    for j in range(2,k+1):
-        jump[i+j]+=1
-for i in range(1,n):
-    r+=jump[i]
-print(r)
+def no_dogs(n):
+    b = [[0] * 2 for i in range(n + 1)]
+    b[0][0],b[0][1] = 1,1
+    for i in range(1, n + 1):
+        b[i][0] = b[i - 1][0] + b[i - 1][1]
+        b[i][1] = b[i - 1][0]
+    return b[len(b) - 1][0]
+
+
+if __name__ == "__main__":
+    n = int(input())
+    print(no_dogs(n))
